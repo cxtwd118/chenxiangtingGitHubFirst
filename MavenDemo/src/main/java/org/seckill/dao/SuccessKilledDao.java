7c38b01b6de5d5a58afb8e055cc5e594c95295fc
chenxiangting <chenxiangting@cmos.chinamobile.com>
2018-10-05 09:23:46 +0800

    秒杀业务的dao层开发

--- MavenDemo/src/main/java/org/seckill/dao/SuccessKilledDao.java
+++ MavenDemo/src/main/java/org/seckill/dao/SuccessKilledDao.java
@@ -1,6 +1,21 @@
 package org.seckill.dao;
 
+import org.seckill.entity.SuccessKilled;
+
 public interface SuccessKilledDao {
+    /**
+     * 插入购买明细，可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return s
+     * 插入行数
+     */
     int insertSuccessKilled(long seckillId, long userPhone);
 
+    /**
+     * 根据id查询SuccessKilled并携带秒杀 产品对象实体
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill (long seckillId);
 }
---
+++ MavenDemo/src/test/java/zx/test/companyProgramTest.java
@@ -0,0 +1,10 @@
+package zx.test;
+
+import java.util.UUID;
+
+public class companyProgramTest {
+    public static void main(String[] args) {
+        String apikey = UUID.randomUUID().toString();
+        System.out.println(apikey);
+    }
+}
